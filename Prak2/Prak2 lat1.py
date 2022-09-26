1956407f9151de0a2a21757e003492a9df6b9635
Dezanvndr <dezanvndra@gmail.com>
2022-09-26 13:07:04 +0700

    Add files via upload

---
+++ Prak2/Prak2 lat1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 26 12:10:33 2022
+
+@author: dezan
+"""
+
+import math
+
+a = float(input("Masukkan nilai a = "))
+b = float(input("Masukkan nilai b = "))
+
+print("Jumlah a ditambah b adalah ", a+b)
+print("Selisih antara a dan b adalah ", abs(b-a))
+print("Jumlah a dikali b adalah ", a*b)
+print("Jumlah sisa pembagian dari hasil a dibagi b adalah ", a%b)
+print("Jumlah a dibagi b adalah ", a/b)
+print("Hasil dari log(a) adalah ", math.log10(a))
+print("Hasil a pangkat b adalah ", a**b)
